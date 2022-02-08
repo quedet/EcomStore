c16d79dc2ea1eebbb4a1ff6c41e00eac117075fc
Fidel Quedet <38511646+quedet@users.noreply.github.com>
2022-02-08 04:23:58 +0000

    Content-Type Builder

---
+++ server/src/api/gategory/controllers/gategory.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  gategory controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::gategory.gategory');
---
+++ server/src/api/gategory/services/gategory.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * gategory service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::gategory.gategory');
